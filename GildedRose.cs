a391064020ee6a505da81cd6d14041f366fdc852
hugolxt <87241914+hugolxt@users.noreply.github.com>
2022-02-05 21:13:19 +0100

    Add files via upload

--- GildedRose.cs
+++ GildedRose.cs
@@ -10,78 +10,86 @@ namespace csharp
             this.Items = Items;
         }
 
+        public void DecreaseQualityForNormalItems(Item item)
+        {
+            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.Quality--;
+            }
+        }
+
+        public void BackStagedPassed(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                if (item.SellIn < 11)
+                {
+                    item.Quality++;
+                }
+
+                if (item.SellIn < 6)
+                {
+                    item.Quality++;
+                }   
+            }
+        }
+
+        public void SellinReachZero(Item item)
+        {
+            if (item.Name != "Aged Brie")
+            {
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality--; // Normal Item
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality = item.Quality - item.Quality;
+                }
+            }
+            else // item is Aged Brie
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+            }
+        } 
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    DecreaseQualityForNormalItems(Items[i]);
                 }
                 else
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Items[i].Quality++;
 
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                            BackStagedPassed(Items[i]);
                         }
                     }
                 }
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    Items[i].SellIn--;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    SellinReachZero(Items[i]);
                 }
             }
         }
